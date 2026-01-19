80ccf5f8781217b6d2d4fbfa342ed38147a1da68
Alexandre G. <alexgleizes18@gmail.com>
2026-01-19 15:15:47 +0100

    Rajout de la page dessin. Sur celle ci on pourra dessiner dans un carré. Ajout d'un sous titre a la page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { Accueil } from './accueil/accueil'; 
 import { MesProjets } from './mes-projets/mes-projets';
+import { Dessin } from './dessin/dessin';
 import { Contact } from './contact/contact';
 
 export const routes: Routes = [
   { path: 'accueil', component: Accueil },
   { path: 'projets', component: MesProjets },
+  { path: 'dessin', component: Dessin },
   { path: 'contact', component: Contact },
   { path: '', redirectTo: 'accueil', pathMatch: 'full' }
 
---
+++ src/app/dessin/dessin.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dessin',
+  imports: [],
+  templateUrl: './dessin.html',
+  styleUrl: './dessin.scss',
+})
+export class Dessin {
+
+}
